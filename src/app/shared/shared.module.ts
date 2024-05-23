492c551da0a366ef1bd994b4c9856544fc8937b0
manuela <manuelajuan26@gmail.com>
2024-05-22 20:48:08 -0500

    se crearon las moleculas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoComponent } from './components/molecules/logo/logo.component';
+import { SearchBarComponent } from './components/molecules/search-bar/search-bar.component';
+import { ButtonProfileComponent } from './components/molecules/button-profile/button-profile.component';
+import { NavComponent } from './components/molecules/nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LogoComponent,
+    SearchBarComponent,
+    ButtonProfileComponent,
+    NavComponent
+  ],
   imports: [
     CommonModule
   ]
