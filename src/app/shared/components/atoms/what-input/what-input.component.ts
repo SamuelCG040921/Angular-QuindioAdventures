40c09113059992b7d5813e33541ef435fcfc7da1
Samuel Calderon <samuelcalderon986@gmail.com>
2024-05-22 20:44:37 -0500

    agregados los atomos necesarios para el modulo de reservas

---
+++ src/app/shared/components/atoms/what-input/what-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-what-input',
+  templateUrl: './what-input.component.html',
+  styleUrl: './what-input.component.scss'
+})
+export class WhatInputComponent {
+
+}
