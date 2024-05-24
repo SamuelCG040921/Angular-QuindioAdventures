be5cf77f1edb2900b94084517bd1ab3af70727a8
SantiCoVe <cottevelasquez@gmail.com>
2024-05-23 22:31:24 -0500

    agregado search-buttom

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ProfileButtonComponent } from './components/atoms/profile-button/profile-button.component';
+import { SearchButtonComponent } from './components/atoms/search-button/search-button.component';
+import { WhatInputComponent } from './components/atoms/what-input/what-input.component';
+import { WhereInputComponent } from './components/atoms/where-input/where-input.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileButtonComponent,
+    SearchButtonComponent,
+    WhatInputComponent,
+    WhereInputComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SearchButtonComponent
   ]
 })
 export class SharedModule { }
