c303b3f9e7c81caf68c756a928153ebd17e92671
Samuel Calderon <samuelcalderon986@gmail.com>
2024-05-23 21:02:02 -0500

    agregado profile-button

---
+++ src/app/shared/components/atoms/profile-img/profile-img.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-img',
+  templateUrl: './profile-img.component.html',
+  styleUrl: './profile-img.component.scss'
+})
+export class ProfileImgComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileImgComponent } from './components/atoms/profile-img/profile-img.component';
+import { RouterModule } from '@angular/router';
+import { ProfileButtonComponent } from './components/atoms/profile-button/profile-button.component';
+import { SearchButtonComponent } from './components/atoms/search-button/search-button.component';
+import { WhatInputComponent } from './components/atoms/what-input/what-input.component';
+import { WhereInputComponent } from './components/atoms/where-input/where-input.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileButtonComponent,
+    ProfileImgComponent,
+    SearchButtonComponent,
+    WhatInputComponent,
+    WhereInputComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+      { path: 'profile', component: SearchButtonComponent }
+    ])
+  ],
+  exports: [
+    ProfileButtonComponent
   ]
 })
 export class SharedModule { }
