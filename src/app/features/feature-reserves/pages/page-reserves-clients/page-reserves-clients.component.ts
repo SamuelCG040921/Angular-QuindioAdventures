181b88d3ac24f3f14dce4a037c3a9ac646bd2ae1
Samuel Calderon <samuelcalderon986@gmail.com>
2024-05-27 20:08:34 -0500

    agregados emptystates

--- src/app/features/feature-reserves/pages/page-reserves-clients/page-reserves-clients.component.ts
+++ src/app/features/feature-reserves/pages/page-reserves-clients/page-reserves-clients.component.ts
@@ -6,9 +6,56 @@ import { Component } from '@angular/core';
   styleUrl: './page-reserves-clients.component.scss'
 })
 export class PageReservesClientsComponent {
-  reserves =[
-    
-  
+  clients = [
+    {
+      id: 1,
+    code: '000043',
+    client: 'Juan Bedoya',
+    date: '24/04/24',
+    adress: 'Barrio las acacias calle 12',
+    email: 'juanbedoya@gmail.com',
+    phone: '3053305459',
+    price: 590000
+    },{
+      id: 2,
+    code: '000044',
+    client: 'Juan Tovar',
+    date: '24/04/24',
+    adress: 'Barrio los quindos calle 13',
+    email: 'juantovar@gmail.com',
+    phone: '30533054379',
+    price: 800000
+    },
+    {
+      id: 3,
+    code: '000045',
+    client: 'Camilo Balsero',
+    date: '24/04/24',
+    adress: 'Barrio villa del carmen calle 16',
+    email: 'camilobalsero16@gmail.com',
+    phone: '3053305459',
+    price: 990000
+    },
+    {
+      id: 1,
+    code: '000043',
+    client: 'Juan Bedoya',
+    date: '24/04/24',
+    adress: 'Barrio las acacias calle 12',
+    email: 'juanbedoya@gmail.com',
+    phone: '3053305459',
+    price: 590000
+    },
+    {
+      id: 1,
+    code: '000043',
+    client: 'Juan Bedoya',
+    date: '24/04/24',
+    adress: 'Barrio las acacias calle 12',
+    email: 'juanbedoya@gmail.com',
+    phone: '3053305459',
+    price: 590000
+    }
   ]
   }
   
