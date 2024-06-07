b5b2881d8af8972e1dda4781716e29aec7ce5c64
Samuel Calderon <samuelcalderon986@gmail.com>
2024-06-07 12:31:42 -0500

    corregidos los estados vacios para funcionar con la mockapi

--- src/app/features/feature-reserves/pages/page-reserves-clients/page-reserves-clients.component.ts
+++ src/app/features/feature-reserves/pages/page-reserves-clients/page-reserves-clients.component.ts
@@ -1,62 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservesService } from '../../services/reserves.service';
 
 @Component({
   selector: 'app-page-reserves-clients',
   templateUrl: './page-reserves-clients.component.html',
   styleUrl: './page-reserves-clients.component.scss'
 })
-export class PageReservesClientsComponent {
-  clients = [
-    {
-      id: 1,
-    code: '000043',
-    client: 'Juan Bedoya',
-    date: '24/04/24',
-    adress: 'Barrio las acacias calle 12',
-    email: 'juanbedoya@gmail.com',
-    phone: '3053305459',
-    price: 590000
-    },{
-      id: 2,
-    code: '000044',
-    client: 'Juan Tovar',
-    date: '24/04/24',
-    adress: 'Barrio los quindos calle 13',
-    email: 'juantovar@gmail.com',
-    phone: '30533054379',
-    price: 800000
-    },
-    {
-      id: 3,
-    code: '000045',
-    client: 'Camilo Balsero',
-    date: '24/04/24',
-    adress: 'Barrio villa del carmen calle 16',
-    email: 'camilobalsero16@gmail.com',
-    phone: '3053305459',
-    price: 990000
-    },
-    {
-      id: 1,
-    code: '000043',
-    client: 'Juan Bedoya',
-    date: '24/04/24',
-    adress: 'Barrio las acacias calle 12',
-    email: 'juanbedoya@gmail.com',
-    phone: '3053305459',
-    price: 590000
-    },
-    {
-      id: 1,
-    code: '000043',
-    client: 'Juan Bedoya',
-    date: '24/04/24',
-    adress: 'Barrio las acacias calle 12',
-    email: 'juanbedoya@gmail.com',
-    phone: '3053305459',
-    price: 590000
-    }
-  ]
+export class PageReservesClientsComponent implements OnInit {
+  reserves: any
+
+constructor(private reservesService:ReservesService){}
+
+ngOnInit() {
+    this.reservesService.getReserves()
+    .subscribe(res=>{
+      this.reserves = res
+    })
+}
   }
   
 
