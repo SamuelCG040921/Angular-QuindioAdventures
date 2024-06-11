7126321a8aef2cca46fb1bffe0a802c4350e77e2
SantiCoVe <cottevelasquez@gmail.com>
2024-06-11 10:23:51 -0500

    correcciones varias

--- src/app/features/feature-reserves/services/reserves.service.ts
+++ src/app/features/feature-reserves/services/reserves.service.ts
@@ -19,29 +19,7 @@ export class ReservesService {
     return this.http.post('http://localhost:8000/reserves', reserva)
   }
 
-  cancelReserve(codigo:any){
-    const updatedReserve:any ={ "estado": "cancelada",
-      "userimg": "https://upload.wikimedia.org/wikipedia/commons/b/bf/Foto_Perfil_.jpg",
-      "numero": 1,
-      "codigo": 33,
-      "tipo": "Chalet",
-      "precio": 1300000,
-      "check-in": "23-04-2024",
-      "check-out": "25-04-2024",
-      "direccion": "Barrio la calzada calle2 casa32",
-      "telefono": "3053305459",
-      "nombre": "Samuel Calderón",
-      "fecharegistro": "24-08-2024",
-      "documento": "1029384651",
-      "cantidadNinos": 1,
-      "cantidadAdultos": 2,
-      "apellido": "Calderón",
-      "correo": "samuelcalderon986@gmail.com",
-      "paymentmethod": "nequi",
-      "paymentmethodimg": "https://mir-s3-cdn-cf.behance.net/projects/404/43a39c151928953.Y3JvcCwxODQ0LDE0NDMsMzYsMA.png",
-      "id": "1c79"}
-    return this.http.put('http://localhost:8000/reserves/', codigo, updatedReserve)
-  }
+  
   getReservebyCode(codigo:any){
     return this.http.get('http://localhost:8000/reserves')
     .pipe(
