e4bc904b9c3314238e80ab4d180b91bfedd8f569
Juan Jose Tovar <juanjosetovar330@gmail.com>
2024-07-12 14:41:08 -0500

    actualizado servicio de reservas

--- src/app/features/feature-reserves/services/reserves.service.ts
+++ src/app/features/feature-reserves/services/reserves.service.ts
@@ -1,9 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
 import { Reserva } from '../models/reserva.model';
 import axios from 'axios';
-
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +9,26 @@ export class ReservesService {
   private apiUrl = 'http://localhost:10101/reserva'; // Asegúrate de que esta URL sea correcta
 
   constructor() {}
-  registerReserva(newReserva: Reserva) {
-    console.log('Sending user data to backend:', newReserva);
 
-    return axios.post(this.apiUrl, newReserva)
-      .then(response => {
-        console.log('Backend response:', response.data);
-        return response.data; // Puedes devolver los datos de respuesta si es necesario
-      })
-      .catch(error => {
-        console.error('Error registering user:', error);
-        throw error;
+  async registerReserva(newReserva: Reserva) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      throw new Error('No token found');
+    }
+
+    console.log('Sending reservation data to backend:', newReserva);
+
+    try {
+      const response = await axios.post(this.apiUrl, newReserva, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
       });
+      console.log('Backend response:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error registering reservation:', error);
+      throw error;
+    }
   }
 }
