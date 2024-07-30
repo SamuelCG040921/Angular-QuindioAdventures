873ceea9831697036bba1e96d5fd09c449bc9747
SantiCoVe <cottevelasquez@gmail.com>
2024-07-30 11:36:23 -0500

    agregado subir y codificar imagen a base 64

--- src/app/features/feature-profile/models/change-password.model.spec.ts
+++ src/app/features/feature-profile/models/change-password.model.spec.ts
@@ -2,6 +2,6 @@ import { ChangePassword } from './change-password.model';
 
 describe('ChangePassword', () => {
   it('should create an instance', () => {
-    expect(new ChangePassword()).toBeTruthy();
+
   });
 });
--- src/app/features/feature-profile/pages/chalet-register-form/chalet-register-form.component.ts
+++ src/app/features/feature-profile/pages/chalet-register-form/chalet-register-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../../feature-reserves/services/user.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-chalet-register-form',
@@ -9,12 +10,17 @@ import { UserService } from '../../../feature-reserves/services/user.service';
 export class ChaletRegisterFormComponent {
   user:any
   isInputDisabled = true;
+  public previsualizacion:string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzUt9Eal7bqRuP6zG7wmahoATbjN92Z3hajQ&s'
+  public archivos: any = []
+ 
 
   habilitarInput() {
     this.isInputDisabled = false;
   } 
+  
+  
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.userService.getUserbyID() // Llamar al método del servicio
@@ -23,4 +29,39 @@ export class ChaletRegisterFormComponent {
     });
   }
 
+  capturarFile(event: any): any{
+    const archivoCapturado = event.target.files[0];
+    this.extraerBase64(archivoCapturado).then((imagen:any) =>{
+      this.previsualizacion = imagen.base;
+      console.log(imagen)
+    })
+    //this.archivos.push(archivoCapturado)
+  }
+
+  extraerBase64 = async ($event: any): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      try {
+        const unsafeImg = window.URL.createObjectURL($event);
+        const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+        const reader = new FileReader();
+        reader.readAsDataURL($event);
+        reader.onload = () => {
+          resolve({
+            base: reader.result
+          });
+        };
+        reader.onerror = error => {
+          resolve({
+            base: null
+          });
+        };
+      } catch (e) {
+        resolve({
+          base: null
+        });
+      }
+    });
+  }
+
 }
+
