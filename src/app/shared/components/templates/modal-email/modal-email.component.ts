88b1c5c9d04b73c5cb76ea4ce9bd0dfe61f73f42
Juan Jose Tovar <juanjosetovar330@gmail.com>
2024-08-10 00:58:44 -0500

    fix ventanas emergentes

--- src/app/shared/components/templates/modal-email/modal-email.component.ts
+++ src/app/shared/components/templates/modal-email/modal-email.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Change } from '../../../../features/feature-login/models/change';
 import { ChangePasswordService } from '../../../../features/feature-profile/services/changepassword.service';
@@ -6,26 +6,28 @@ import { ChangePasswordService } from '../../../../features/feature-profile/serv
 @Component({
   selector: 'app-modal-email',
   templateUrl: './modal-email.component.html',
-  styleUrl: './modal-email.component.scss'
+  styleUrls: ['./modal-email.component.scss']  // Corregido de `styleUrl` a `styleUrls`
 })
-export class ModalEmailComponent {
+export class ModalEmailComponent implements OnInit {
   @Output() close = new EventEmitter<void>();
   emailForm!: FormGroup;
   isSubmitting: boolean = false;
+  isAlertOpen = false;
+  isErrorAlertOpen = false;
 
   constructor(private fb: FormBuilder, private changeService: ChangePasswordService) {}
 
-  closeModal(): void {
-    this.close.emit();
-  }
-
-  ngOnInit() : void {
+  ngOnInit(): void {
     this.emailForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
     });
   }
 
-  onSubmit() {
+  closeModal(): void {
+    this.close.emit();
+  }
+
+  onSubmit(): void {
     if (this.emailForm.valid) {
       this.isSubmitting = true; // Deshabilitar el botón al enviar
 
@@ -37,20 +39,40 @@ export class ModalEmailComponent {
         response => {
           this.isSubmitting = false; // Habilitar el botón después de la respuesta
           console.log('Envio exitoso:', response);
-          // Manejar la respuesta exitosa aquí (p.ej., redirigir al usuario)
+          this.openAlert();
         }
       ).catch(
         error => {
           this.isSubmitting = false; // Habilitar el botón después de un error
           console.error('Error:', error);
+          this.openErrorAlert();
         }
       );
-
-      
     } else {
       console.error('Form is not valid');
+      this.openErrorAlert();
       this.emailForm.markAllAsTouched(); // Marcar todos los campos como tocados para mostrar errores
     }
   }
 
+  openAlert(): void {
+    this.isAlertOpen = true;
+  }
+
+  closeAlert(): void {
+    this.isAlertOpen = false;
+  }
+
+  openErrorAlert(): void {
+    this.isErrorAlertOpen = true;
+  }
+
+  closeErrorAlert(): void {
+    this.isErrorAlertOpen = false;
+  }
+
+  isFieldInvalid(field: string): boolean {
+    const control = this.emailForm.get(field);
+    return control ? !control.valid && (control.dirty || control.touched) : false;
+  }
 }
