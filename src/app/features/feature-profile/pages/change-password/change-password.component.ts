0c00fdc62d9787ec2e1df4ea0ce0cf9bf7a97607
Juan Jose Tovar <juanjosetovar330@gmail.com>
2024-08-09 19:42:04 -0500

    Se añadio logica para envio de correo para cambio de contraseña

--- src/app/features/feature-profile/pages/change-password/change-password.component.ts
+++ src/app/features/feature-profile/pages/change-password/change-password.component.ts
@@ -2,26 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ChangePasswordService } from '../../services/changepassword.service';
 import { ChangePassword } from '../../models/change-password.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.scss'
 })
-export class ChangePasswordComponent implements OnInit{
+export class ChangePasswordComponent implements OnInit {
   isAlertOpen = false;
   isErrorAlertOpen = false;
   changeForm!: FormGroup;
   isSubmitting: boolean = false;
+  token!: string | null;
 
-  constructor(private fb: FormBuilder, private changePasswordServices: ChangePasswordService) {}
+  constructor(
+    private fb: FormBuilder,
+    private changePasswordService: ChangePasswordService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    // Obtiene el token desde la URL
+    this.token = this.route.snapshot.queryParamMap.get('token');
+
     this.changeForm = this.fb.group({
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
     });
+
+    if (!this.token) {
+      // Si no hay token, redirige o muestra un mensaje de error
+      console.error('Token no proporcionado');
+    }
   }
+
   openAlert(): void {
     this.isAlertOpen = true;
   }
@@ -43,38 +59,36 @@ export class ChangePasswordComponent implements OnInit{
     return control ? !control.valid && (control.dirty || control.touched) : false;
   }
 
-  isWarningAlertOpen = false
+  isWarningAlertOpen = false;
 
   closeWarningAlert(){
-    this.isWarningAlertOpen = false
+    this.isWarningAlertOpen = false;
   }
 
   openWarningAlert(){
-    this.isWarningAlertOpen = true
+    this.isWarningAlertOpen = true;
   }
 
-  onSubmit() {
-    if(this.changeForm.valid){
-      
+  async onSubmit() {
+    if (this.changeForm.valid && this.token) {
       const changeData = new ChangePassword(
         this.changeForm.value.password,
         this.changeForm.value.confirmPassword
       );
 
-      this.changePasswordServices.change(changeData).then(
-        response =>{
-          console.log('Cambio exitoso:', response);
-        } 
-      ).catch(
-        error => {
-          console.log('Cambio fallido:', error);
-          
-        }
-      )
-    }else{
+      try {
+        const response = await this.changePasswordService.change(changeData, this.token);
+        
+        console.log('Cambio exitoso:', response);
+        this.openAlert();
+      } catch (error) {
+        console.error('Cambio fallido:', error);
+        this.openErrorAlert();
+      }
+    } else {
       console.error('Form is not valid');
       this.changeForm.markAllAsTouched();
+      this.openWarningAlert();
     }
   }
-
 }
--- src/app/shared/components/templates/modal-email/modal-email.component.ts
+++ src/app/shared/components/templates/modal-email/modal-email.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Change } from '../../../../features/feature-login/models/change';
+import { ChangePasswordService } from '../../../../features/feature-profile/services/changepassword.service';
 
 @Component({
   selector: 'app-modal-email',
@@ -7,9 +10,47 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class ModalEmailComponent {
   @Output() close = new EventEmitter<void>();
+  emailForm!: FormGroup;
+  isSubmitting: boolean = false;
+
+  constructor(private fb: FormBuilder, private changeService: ChangePasswordService) {}
 
   closeModal(): void {
     this.close.emit();
+  }
+
+  ngOnInit() : void {
+    this.emailForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    if (this.emailForm.valid) {
+      this.isSubmitting = true; // Deshabilitar el botón al enviar
+
+      const changeData = new Change(
+        this.emailForm.value.email,
+      );
+
+      this.changeService.solicitarRestablecimiento(changeData).then(
+        response => {
+          this.isSubmitting = false; // Habilitar el botón después de la respuesta
+          console.log('Envio exitoso:', response);
+          // Manejar la respuesta exitosa aquí (p.ej., redirigir al usuario)
+        }
+      ).catch(
+        error => {
+          this.isSubmitting = false; // Habilitar el botón después de un error
+          console.error('Error:', error);
+        }
+      );
+
+      
+    } else {
+      console.error('Form is not valid');
+      this.emailForm.markAllAsTouched(); // Marcar todos los campos como tocados para mostrar errores
+    }
   }
 
 }
