a42ceb7caddc1b6bba78de6e210164d8abc2339b
manuela <manuelajuan26@gmail.com>
2024-09-04 22:52:21 -0500

    se cambiaron algunos responsives

--- src/app/shared/components/molecules/nav-logueado/nav-logueado.component.ts
+++ src/app/shared/components/molecules/nav-logueado/nav-logueado.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './nav-logueado.component.scss'
 })
 export class NavLogueadoComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  disguiseMenu(){
+    this.isMenuOpen = false;
+  }
 }
